af130e7f136788fb0f7a93f5c47dd189c4e7157c
anushaferoz123 <94545979+anushaferoz123@users.noreply.github.com>
2022-04-04 00:58:06 +0500

    MyRestfulMind-sprint2

--- app/src/main/java/com/example/myrestfulmind/ClienthomeActivity.kt
+++ app/src/main/java/com/example/myrestfulmind/ClienthomeActivity.kt
@@ -3,11 +3,13 @@ package com.example.myrestfulmind
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class ClienthomeActivity: AppCompatActivity() {
+    private lateinit var btnSurvey: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_clienthome)
@@ -16,6 +18,11 @@ class ClienthomeActivity: AppCompatActivity() {
         imageSignout.setOnClickListener(View.OnClickListener {
             val regIntent = Intent(this@ClienthomeActivity, MainActivity::class.java)
             startActivity(regIntent)
+        })
+        btnSurvey = findViewById(R.id.btnSurvey)
+        btnSurvey.setOnClickListener(View.OnClickListener {
+            val registerIntent = Intent(this@ClienthomeActivity, surveyOneActivity::class.java)
+            startActivity(registerIntent)
         })
     }
 }
